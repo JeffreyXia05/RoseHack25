232d68c1977c74ebbf8fcc03d7f18607f44bfa7a
Jeffrey <Jeffx2016@gmail.com>
2025-01-25 20:13:29 -0800

    new website

---
+++ bruh/src/app/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex flex-col bg-green-950 h-screen font-mono'>
+      <div className='text-end text-5xl text-green-400 pr-10 pt-10'>
+        Carbon Emission
+      </div>
+      <div className='text-end text-gray-400 pr-10 pt-2'>
+        Blash blnashs fiosdjfijs sdkljfhsdof sdkh clsdjlfs dj flskjkl vlsl vi jsd.
+      </div>
+      <div className='text-center ml-19'>
+        Statistics
+      </div>
+      <div className='align- ml-19'>
+        slkdfjlskdjf sodjfl skdjf lksjfl kesj lflsdflskjdflkskj flsdj flksj flksjdlf slkj dlfj slkdfj leij rbaolsdj flskj flsh odifh lskdjf lkwejf oisj dflksj fjweolhfos dfjsl fjosidhf piauhf lsjkd n;c apiesuh fpsiduhfpi
+      </div>
+    </div>
+  )
+}
+
+export default page
