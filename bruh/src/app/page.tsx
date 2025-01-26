48927da7ecbb10240b50dda864919574b2deb2d4
Jeffrey <Jeffx2016@gmail.com>
2025-01-25 20:42:28 -0800

    added image

--- bruh/src/app/page.tsx
+++ bruh/src/app/page.tsx
@@ -1,20 +1,27 @@
 import React from 'react'
+import CEIcon from "../../public/5672000.png"
 
 const page = () => {
   return (
-    <div className='flex flex-col bg-green-950 h-screen font-mono'>
-      <div className='text-end text-5xl text-green-400 pr-10 pt-10'>
-        Carbon Emission
+    <div className='flex flex-row'>
+      <div> 
+        <Image src={CEIcon} alt="Logo"/>
       </div>
-      <div className='text-end text-gray-400 pr-10 pt-2'>
-        Blash blnashs fiosdjfijs sdkljfhsdof sdkh clsdjlfs dj flskjkl vlsl vi jsd.
-      </div>
-      <div className='text-center ml-19'>
-        Statistics
-      </div>
-      <div className='align- ml-19'>
-        slkdfjlskdjf sodjfl skdjf lksjfl kesj lflsdflskjdflkskj  flksj flksjdlf slkj dlfj slkdfj leij rbaolsdj flskj flsh odifh lskdjf lkwejf oisj dflksj fjweolhfos dfjsl fjosidhf piauhf lsjkd n;c apiesuh fpsiduhfpi
-      </div>
+      <div className='relative flex flex-col bg-green-950 h-screen font-mono'>
+        <div className='text-end text-5xl text-green-400 pr-10 pt-10'>
+          Carbon Emission
+        </div>
+        <div className='text-end text-gray-200 pr-10 pt-2'>
+          Blash blnashs fiosdjfijs sdkljfhsdof sdkh clsdjlfs dj flskjkl vlsl vi jsd.
+        </div>
+        <div className='ml-[49%] text-4xl text-green-400 pt-10'>
+          Statistics
+        </div>
+        <div className='ml-[49%] pt-2 text-gray-200'>
+          slkdfjlskdjf sodjfl skdjf lksjfl kesj lflsdflskjdflkskj  flksj flksjdlf slkj dlfj slkdfj leij rbaolsdj flskj flsh odifh lskdjf lkwejf oisj dflksj fjweolhfos dfjsl fjosidhf piauhf lsjkd n;c apiesuh fpsiduhfpi
+        </div>
+    </div>
+
     </div>
   )
 }
