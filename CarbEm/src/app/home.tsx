a129d35e8840b2f52090be3a11b363984e5bc30a
Jeffrey <Jeffx2016@gmail.com>
2025-01-25 16:55:39 -0800

    can run now with skeleton

---
+++ CarbEm/src/app/home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className = "flex flex-col">Top Contributers of Carbon Emission</div>
+  )
+}
+
+export default home
---
+++ CarbEm/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Home from "./home";
+import Reduce from "./reduce";
+import React from 'react';
+
+const page = () => {
+  return (
+      <div>
+        <Home/>
+        <Reduce />
+    </div>
+  );
+};
+
+export default page
